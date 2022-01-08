0a754353246e566b668525811d887d7820f0d368
pjsingal <59570055+pjsingal@users.noreply.github.com>
2022-01-08 11:45:00 -0500

    deleted timeline code (**high odds of error here)
    project.name --> project.title
    project.position --> project.subtitle
    deleted yearMajor, gitHub, linkedIn, email

--- src/routes/index.json.js
+++ src/routes/index.json.js
@@ -7,33 +7,20 @@ function getData() {
     .filter(project => !project.hidden)
     .map(project => {
       return {
-        name: project.name,
-        position: project.position,
-        yearMajor: project.yearMajor,
+        name: project.title,
+        position: project.subtitle,
         image: project.image,
-        gitHub: project.gitHub,
-        linkedIn: project.linkedIn,
-        email: project.email,
         blurb: project.blurb
       };
     });
 
-  const timelineFile = require("../../../content/timeline.json");
-
-  const timeline = timelineFile
-    .map(item => ({ ...item, date: `${item.date}-02` }))
-    .map(item => ({
-      prettyDate: dateformat(item.date, "mmmm yyyy"),
-      ...item
-    }))
-    .sort((a, b) => new Date(a.date) - new Date(b.date));
-  return { portfolio, timeline };
+  return { portfolio };
 }
 
 export function get(req, res) {
   res.writeHead(200, {
     "Content-Type": "application/json"
   });
-  const { portfolio, timeline } = getData();
-  res.end(JSON.stringify({ portfolio, timeline }));
+  const { portfolio } = getData();
+  res.end(JSON.stringify({ portfolio }));
 }
