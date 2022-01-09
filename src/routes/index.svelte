2a67c35a6da2e9732c49cefcc17314befb6baff0
pjsingal <59570055+pjsingal@users.noreply.github.com>
2022-01-09 03:39:28 -0500

    updates

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -19,11 +19,6 @@
   <p>I conduct research with the Rival Lab at Queen&#x27;s University, which focuses on bio-inspired experimental fluid dynamics. My first project was designing a complex CAD model of an airfoil, which contains an embedded network of pressure-measurement channels, taps, and transducer ports that are geometrically optimized to reduce noise and signal error. Currently, I am assisting with running towing tank experiments on airfoils across a range of boundary conditions to collect data for training a predictive algorithm for sparse pressure data in gusty, turbulent flows.</p>
   <p>For my capstone project, I am involved in a year-long industry partnership with a manufacturer of heavy-duty electric vehicles. Our team is designing an optimized thermal management system that minimizes redundancy while maintaining the sensitive operating temperatures of Li-ion batteries, motors, and auxiliary components. We validate this design using multi-objective optimization techniques, which are computed using simulations and numerical models that we have developed. I am also completing an undergraduate thesis that involves using ANSYS Fluent to simulate turbulent, compressible flow through a rotating detonation engine fuel supply valve.</p>
   <p>Further information about these projects and other experiences is available in my CV and project summary. I am always looking to connect with like-minded people and exchange ideas. Please feel free to reach out!</p>
-
-  <script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
-  <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="patricksingal" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://ca.linkedin.com/in/patricksingal?trk=profile-badge">Patrick Singal</a></div>
-
   <h2>Contact Information</h2>
-  <p>Email: p.singal@queensu.ca</p>
-  <p>Phone: 647-938-8135</p>
+  <p>Email: p.singal@queensu.ca<br/>Phone: 647-938-8135</p>
 </div>
